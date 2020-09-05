e7fab42a8fb87770501bf774f8734a6498d69a39
wangheng <wangheng@mcep.net.cn>
2020-09-05 21:59:14 +0800

    new

---
+++ src/main/java/com/kit/algorithmlearning/AlgorithmLearningApplication.java
@@ -0,0 +1,13 @@
+package com.kit.algorithmlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmLearningApplication.class, args);
+    }
+
+}
